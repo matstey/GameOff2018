999189c16b575988aa66d20f7cf7ee5421090306
Andy <agates01@hotmail.com>
2018-11-05 18:58:44 +0000

    Replaced with a quick unity project

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    Transform m_target;
+
+    [SerializeField]
+    float m_smoothTime = 0.3f;
+
+    Vector3 m_velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (m_target != null)
+        {
+            Vector3 targetPosition = m_target.TransformPoint(new Vector3(0, 0, transform.position.z));
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_velocity, m_smoothTime);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float m_maxSpeed = 10.0f;
+
+    [SerializeField]
+    float m_maxWalkAnimSpeed = 0.5f;
+
+    [SerializeField]
+    Animator m_bodyAnimController;
+
+    [SerializeField]
+    Animator m_legsAnimController;
+    
+    Rigidbody2D m_rigidbody;
+    SpriteRenderer[] m_renderers;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_renderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        bool attack = Input.GetButtonDown("Fire1");
+
+        Vector2 move = new Vector2(moveX, moveY);
+        m_rigidbody.velocity = move * m_maxSpeed;
+
+        float animSpeed = move.magnitude * m_maxWalkAnimSpeed;
+
+        if (animSpeed > 0.1)
+        {
+            m_legsAnimController.speed = animSpeed;
+            m_bodyAnimController.speed = animSpeed;
+
+        }
+        else
+        {
+            m_legsAnimController.Play("LegsRun", -1, 0.0f);
+        }
+
+        SetDirection(moveX < 0);
+
+        if (attack)
+        {
+            m_bodyAnimController.SetTrigger("Attack");
+        }
+    }
+
+    void SetDirection(bool left)
+    {
+        foreach (SpriteRenderer r in m_renderers)
+        {
+            r.flipX = left;
+        }
+    }
+}
